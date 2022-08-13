c5ddb5f416df378d87b2cd982322b0cf49bf293b
Kaifas Stromai <storm.cephas@outlook.com>
2022-08-13 14:48:15 -0500

    start again

---
+++ novellex/novellexApp~.swift
@@ -0,0 +1,18 @@
+//
+//  novellexApp.swift
+//  novellex
+//
+//  Created by Κάϊφας Στρομαί on 6/4/22.
+//
+
+import SwiftUI
+import Alamofire
+@main
+struct novellexApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
